7265f24ee3b1b4aeebd72a0c99a63774e3366473
ochoa07 <och23008@byui.edu>
2025-09-25 17:27:18 -0600

    templatesmjs

--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -1,12 +1,13 @@
-export function parkInfoTemplate(info){
-    return `<a href="/" class="hero-banner__title">${info.name}</a>
+export function parkInfoTemplate(info) {
+  return `<a href="/" class="hero-banner__title">${info.name}</a>
     <p class="hero-banner__subtitle">
-    <span>${info.designation}</span>
-    <span>${info.states}</span>
+      <span>${info.designation}</span>
+      <span>${info.states}</span>
     </p>`;
 }
+
 export function mediaCardTemplate(info) {
-     return `<div class="media-card">
+  return `<div class="media-card">
     <a href="${info.link}">
     <img src="${info.image}" alt="${info.name}" class="media-card__img">
     <h3 class="media-card__title">${info.name}</h3>
